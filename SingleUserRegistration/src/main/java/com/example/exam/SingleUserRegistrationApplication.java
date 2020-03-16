470bd3df55538e1ae56379e4156676c4f28ef6e3
riya.a.dutta <riya.a.dutta@KDC1-L-PF1BK89G.dir.svc.accenture.com>
2020-03-16 14:57:44 +0530

    "Commit test comment"

--- SingleUserRegistration/src/main/java/com/example/exam/SingleUserRegistrationApplication.java
+++ SingleUserRegistration/src/main/java/com/example/exam/SingleUserRegistrationApplication.java
@@ -3,7 +3,7 @@ package com.example.exam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
+//test
 @EnableEurekaClient
 @SpringBootApplication
 public class SingleUserRegistrationApplication {
